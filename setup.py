99fae3cecc3b7f37d3edbdcb1a0d0841979a0520
Stas Bekman <stas@stason.org>
2018-11-14 17:55:57 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+with open('README.md') as readme_file: readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = ['Click>=6.0', ]
+requirements = [ ]
 
 setup_requirements = [ ]
 
@@ -25,23 +18,13 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     description="experiment containers for jupyter/ipython",
-    entry_points={
-        'console_scripts': [
-            'ipyexperiments=ipyexperiments.cli:main',
-        ],
-    },
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='ipyexperiments',
     name='ipyexperiments',
