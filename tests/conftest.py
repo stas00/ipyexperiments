54becc95bd9096ba9fefee880a8a6311ce41cd43
Stas Bekman <stas@stason.org>
2020-11-16 20:06:29 -0800

    test suite needs pytest-ipynb

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,10 @@ from os.path import abspath, dirname
 # second in sys.path (the first path is the test dir path)
 git_repo_path = abspath(dirname(dirname(__file__)))
 sys.path.insert(1, git_repo_path)
+
+# make sure we have pytest-ipynb installed - otherwise half the test suite will be not collected and run
+try:
+    import pytest_ipynb
+except:
+    raise ModuleNotFoundError("to run the test suite, run: "
+                              "pip install git+https://github.com/stas00/pytest-ipynb.git")
