d69c4c90def6325c99027ddadfbe45bf267e5ca4
Stas Bekman <stas@stason.org>
2021-02-13 11:09:02 -0800

    small improvement

--- tests/test_utils_mem.py
+++ tests/test_utils_mem.py
@@ -3,10 +3,10 @@ from ipyexperiments.utils.mem import *
 from math import isclose
 
 def test_leave_free_mbs():
-    free_before = gpu_mem_get_free_mbs()
-    assert free_before, "have some free gpu memory"
-
     delta = 10
+    free_before = gpu_mem_get_free_mbs()
+    assert free_before > delta, "have some free gpu memory"
+
     left = free_before - delta
     buf = gpu_mem_leave_free_mbs(left)
     assert buf is not None, f"able to allocate {delta} mem"
